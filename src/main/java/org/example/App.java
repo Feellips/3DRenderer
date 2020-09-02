353c915d3ddb583fe0494972219b0bacfa2dafb5
Philipp Phedyushin <feelsveryverybad@gmail.com>
2020-09-03 01:07:53 +0300

    add starfield intro

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,12 +2,21 @@ package org.example;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Display display = new Display(800,600,"3D Renderer");
+public class App {
+    public static void main(String[] args) {
+        Display display = new Display(800, 600, "3D Renderer");
+        Bitmap target = display.GetFrameBuffer();
+        Star3D star3D = new Star3D(2048, 64.0f, 20.0f);
+
+        long previousTime = System.nanoTime();
+        while (true) {
+            long currentTime = System.nanoTime();
+
+            float delta = (float) ((currentTime - previousTime) / 1000000000.0);
+            previousTime = currentTime;
+            star3D.UpdateAndRender(target, delta);
+            display.SwapBuffers();
+        }
     }
 }
--- src/main/java/org/example/Display.java
+++ src/main/java/org/example/Display.java
@@ -1,17 +1,33 @@
 package org.example;
 
 import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferByte;
 import javax.swing.JFrame;
 
 public class Display extends Canvas {
 
     private final JFrame m_frame;
+    private final Bitmap m_frameBuffer;
+    private final BufferedImage m_displayImage;
+    private final byte[] m_displayComponents;
+    private final BufferStrategy m_bufferStrategy;
+    private final Graphics m_graphics;
 
     public Display(int width, int height, String title) {
         Dimension size = new Dimension(width, height);
         setPreferredSize(size);
         setMinimumSize(size);
         setMaximumSize(size);
+
+        m_frameBuffer = new Bitmap(width, height);
+        m_displayImage = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+        m_displayComponents = ((DataBufferByte) m_displayImage.getRaster().getDataBuffer()).getData();
+
+        m_frameBuffer.Clear((byte)0x80);
+        m_frameBuffer.DrawPixel(100, 100, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0xFF);
 
         m_frame = new JFrame();
 
@@ -22,6 +38,19 @@ public class Display extends Canvas {
         m_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         m_frame.setVisible(true);
 
+        createBufferStrategy(1);
+        m_bufferStrategy = getBufferStrategy();
+        m_graphics = m_bufferStrategy.getDrawGraphics();
+
     }
 
+    public void SwapBuffers() {
+        m_frameBuffer.CopyToByteArray(m_displayComponents);
+        m_graphics.drawImage(m_displayImage, 0, 0, m_frameBuffer.GetWidth(), m_frameBuffer.GetHeight(), null);
+        m_bufferStrategy.show();
+    }
+
+    public Bitmap GetFrameBuffer() {
+        return m_frameBuffer;
+    }
 }
